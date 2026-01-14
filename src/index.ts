819d496e5b0d2c0472c1312864c784dfd3d4b585
Juan Pablo Sarmiento <pablo@avandarlabs.com>
2026-01-14 17:38:22 -0500

    Exporting types now

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export { Acclimate } from './Acclimate';
+export { Acclimate } from "./Acclimate";
+export type {
+  IAcclimateCLI,
+  CLIParamDataType,
+  AnyCLI,
+  CLICommandName,
+  CLIFullOptionName,
+  CLIOptionAlias,
+  CLIPositionalParam,
+  CLIOptionParam,
+} from "./AcclimateCLI/AcclimateCLI.types";
